84be4168a239f6a66fb3a189234b282d5cb39768
leahzahn <zahn@chapman.edu>
2021-02-12 18:13:19 -0800

    please work

---
+++ Assets/GameManagerScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class GameManagerScript : MonoBehaviour
+{
+    public string filepath;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateFile();
+        WriteFile("Application startup");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void CreateFile()
+    {
+        if (!File.Exists(filepath))
+        {
+            File.Create(filepath).Close();
+        }
+    }
+
+    private void WriteFile(string text)
+    {
+        using (StreamWriter writer = new StreamWriter(filepath))
+        {
+            writer.WriteLine(text);
+        }
+    }
+
+}
